b91e4c2562537f8094a28feda946fed6fd844c3a
Md.Minhazul Haque <ashim.ashim7470@gmail.com>
2021-10-03 23:57:44 +0600

    fonts and router and bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navigation from './Navigation/Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation></Navigation>
     </div>
   );
 }
