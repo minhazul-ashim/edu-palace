dc73bbfae4868061834995365a75f2ff5e25f676
Md.Minhazul Haque <ashim.ashim7470@gmail.com>
2021-10-04 16:38:39 +0600

    some more comments added

--- src/hooks/useCourse.js
+++ src/hooks/useCourse.js
@@ -1,9 +1,11 @@
 import useCourses from "./useCourses"
 
+//This function is used for finding a specific course by their id and return the course;
+
 const useCourse = (id) => {
 
     const [courses] = useCourses();
-    return courses.find( course => course.id == id);
+    return courses.find(course => course.id == id);
 }
 
 export default useCourse;
--- src/hooks/useCourses.js
+++ src/hooks/useCourses.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react"
 
+
+//This function is a custom react hook, which is used for getting all the course information;
+
 const useCourses = () => {
 
     const [courses, setCourses] = useState([]);
