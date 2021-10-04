5db1b5d74592a1ab555e7a14994187793f375366
Md.Minhazul Haque <ashim.ashim7470@gmail.com>
2021-10-04 16:48:42 +0600

    course chooser completed

--- src/components/EnrollForm/EnrollForm.js
+++ src/components/EnrollForm/EnrollForm.js
@@ -1,9 +1,15 @@
 import './EnrollForm.css'
 import React from 'react';
+import { Form } from 'react-bootstrap';
+import useCourses from '../../hooks/useCourses';
 
 //No functions or hooks has been used here. All the information are hard coded;
 
 const EnrollForm = () => {
+
+    const [courses] = useCourses();
+
+    console.log(courses)
 
     return (
         <div className='enroll-bg'>
@@ -13,6 +19,14 @@ const EnrollForm = () => {
                     <input className='form-control mb-3' type="email" placeholder='Enter your email' />
                     <input className='form-control mb-3' type="number" placeholder='Enter your age' />
                     <input className='form-control mb-3' type="number" placeholder='Enter your institution name' />
+                    <Form.Select aria-label="Floating label select example" className='mb-2'>
+                        <option>Choose your course</option>
+                        {
+                            courses.map(course => {
+                                return <option value="1" key={course.id}>{course.course}</option>
+                            })
+                        }
+                    </Form.Select>
                     <button className='btn btn-danger'> Submit</button>
                 </form>
             </div>
